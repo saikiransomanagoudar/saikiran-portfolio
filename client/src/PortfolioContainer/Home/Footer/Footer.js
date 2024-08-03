397fa27f8e7458186cd7c6bb3136216364adfba0
saiki12 <saikiranhs338@gmail.com>
2024-08-03 13:58:30 -0500

    footer update

--- client/src/PortfolioContainer/Home/Footer/Footer.js
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div className='footer-container'>
             <div className='footer-parent'>
-                <img src={require ('../../../assets/Home/shape-bg.png')} alt='no internet connection'/>
+                {/* <img src={require ('../../../assets/Home/shape-bg.png')} alt='no internet connection'/> */}
             </div>
         </div>
     )
