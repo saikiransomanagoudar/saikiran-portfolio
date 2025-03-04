acb62bbe8e24d6a315a4684d0ef193e44a62707e
saiki12 <saikiranhs338@gmail.com>
2025-03-04 14:04:00 -0600

    Minor App.js update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// import {useTypewriter, Cursor} from 'react-simple-typewriter'
 import React from 'react';
 import './App.css';
 import PortfolioContainer from './PortfolioContainer/PortfolioContainer';
